be7d2a09e5fabf196abb30c97218b20e741a55a8
igorMori <igor_canada@outlook.com>
2019-06-02 22:22:27 -0400

    file

---
+++ src/portal.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+class portal extends React.Component {
+    state = {  }
+    render() { 
+        return  <div>
+
+                ISDashdosahdashdusa
+        </div>
+         ;
+    }
+}
+ 
+export default portal;  
