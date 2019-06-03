ddc3b612a19acbb80b6a23b63576eaee26c7baaa
igorMori <igor_canada@outlook.com>
2019-06-02 22:40:07 -0400

    fixed
    fixed

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import {BrowserRouter, Route, Switch} from 'react-router-dom'
+import Portal from './portal'
 
 function App() {
   return (
     <div className="App">
      <BrowserRouter>
         <Switch> 
-            <Route path="/user" component={Userportal}/>
-            <Route path="/" component={Initialportal}/>
+            <Route path="/user" component={Portal}/>
         </Switch>
     </BrowserRouter>
     </div>
